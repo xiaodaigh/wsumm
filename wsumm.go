8c400ebc4ba1446033475adb0dfa4a58ded5f3bf
susabi <zhuojia.dai@gmail.com>
2017-08-13 16:16:32 +1000

    initial

---
+++ wsumm.go
@@ -0,0 +1,24 @@
+package wsumm
+
+import "github.com/gorilla/websocket"
+
+type Conn struct {
+	Conn           *websocket.Conn
+	readSemaphore  chan bool
+	writeSemaphore chan bool
+}
+
+//
+func (c *Conn) WriteJSON(v interface{}) error {
+	c.writeSemaphore <- true
+	res := c.Conn.WriteJSON(v)
+	<-c.writeSemaphore
+	return res
+}
+
+func (c Conn) WriteMessage(messageType int, data []byte) error {
+	c.writeSemaphore <- true
+	res := c.Conn.WriteMessage(messageType data)
+	<-c.writeSemaphore
+	return res
+}
