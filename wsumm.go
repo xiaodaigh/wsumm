a519fbf3c51f784527d31028bd0192f9a6e02e7c
susabi <zhuojia.dai@gmail.com>
2017-08-13 21:20:18 +1000

    implemented a semaphore for write

--- wsumm.go
+++ wsumm.go
@@ -1,24 +1,52 @@
 package wsumm
 
-import "github.com/gorilla/websocket"
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+)
 
 type Conn struct {
-	Conn           *websocket.Conn
+	*websocket.Conn
 	readSemaphore  chan bool
 	writeSemaphore chan bool
 }
 
-//
+type Upgrader struct {
+	*websocket.Upgrader
+}
+
+func (c *Conn) createWriteSemaphore() {
+	tmp := make(chan bool, 1)
+	c.writeSemaphore = tmp
+}
+
+func (c *Conn) createReadSemaphore() {
+	tmp := make(chan bool, 1)
+	c.readSemaphore = tmp
+}
+
 func (c *Conn) WriteJSON(v interface{}) error {
+	if c.readSemaphore == nil {
+		c.createWriteSemaphore()
+	}
 	c.writeSemaphore <- true
 	res := c.Conn.WriteJSON(v)
 	<-c.writeSemaphore
 	return res
 }
 
-func (c Conn) WriteMessage(messageType int, data []byte) error {
+func (c *Conn) WriteMessage(messageType int, data []byte) error {
+	if c.readSemaphore == nil {
+		c.createWriteSemaphore()
+	}
 	c.writeSemaphore <- true
 	res := c.Conn.WriteMessage(messageType, data)
 	<-c.writeSemaphore
 	return res
+}
+
+func (u *Upgrader) Upgrade(w http.ResponseWriter, r *http.Request, responseHeader http.Header) (*Conn, error) {
+	c, e := u.Upgrader.Upgrade(w, r, responseHeader)
+	cc := &Conn{Conn: c}
+	return cc, e
 }
